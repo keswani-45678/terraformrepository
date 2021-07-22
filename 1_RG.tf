3b22fb00197c2eebce7ce01ceb413595e58569f7
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-07-22 17:33:38 +0530

    Update 1_RG.tf

--- 1_RG.tf
+++ 1_RG.tf
@@ -2,6 +2,16 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "remote" {
+    organization = "terraformrepo"
+
+    workspaces {
+      name = "cliworkspacetf"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "resource_group" {
   
   name     = "${var.namespace}-rg-${var.env}"
