83cb1750501c89fa4235729f5c9049ed8436a0db
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-07-23 12:58:00 +0530

    Update 1_RG.tf

--- 1_RG.tf
+++ 1_RG.tf
@@ -7,3 +7,14 @@ terraform {
     }
   }
 }
+
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "tests"
+  location = "eastus"
+}
+
+
+output "rg-name"{
+        value = "${azurerm_resource_group.resource_group.name}"
+}
