f7545c1b5a3566d744d0413fdaeab8dd0dfb3701
keswani-45678 <87806290+keswani-45678@users.noreply.github.com>
2021-07-27 17:42:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,13 @@ provider "azurerm" {
   features {}
 }
 
+  
+
+
 module "tfdemo" {
-  source  = "app.terraform.io/terraformrepo/tfdemo/azurerm"
-  version = "1.0.1"
+  source  = "app.terraform.io/terraformrepo/tfdemo/azure"
+  version = "1.0.6"
+
+  network_interfaces = [         {             name = "testnic1",             rgName = "test1",             location = "east us",             private_ip = "10.0.1.4",             vnetname   = "example-vnet",             subnetName = "example-subnet"         },         {             name = "testnic2",             rgName = "test1",             location = "east us",             private_ip = "10.0.1.8",             vnetname   = "example-vnet",             subnetName = "example-subnet"         }     ]
+  subscriptionid = "c961a351-ec79-490c-89d5-b31ef157ec0a"
 }
